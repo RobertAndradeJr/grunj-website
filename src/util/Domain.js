6749017a276e81befe4f1ae4b527e3694410812a
Robert Andrade <sandrade@wiley.com>
2020-07-03 19:50:44 -0500

    try to fix domain last time today

--- src/util/Domain.js
+++ src/util/Domain.js
@@ -1,2 +1,2 @@
 export const development = process.env.NODE_ENV === 'development';
-export const websiteURL = development ? '/' : process.env.REACT_APP_WEBSITE_URL
+export const websiteURL = development ? '/' : 'grunj.netlify.app'
